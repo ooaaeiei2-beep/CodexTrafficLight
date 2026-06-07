984ab0b3ea45071c139f8209bf46616e0206f85d
ooaaeiei2-beep <ooaaeiei2@gmail.com>
2026-06-07 17:21:47 +0800

    更新版本号到 1.0.1

--- make_dmg.sh
+++ make_dmg.sh
@@ -4,7 +4,7 @@ set -e
 PROJECT_DIR="$(cd "$(dirname "$0")" && pwd)"
 WORKSPACE_DIR="$(dirname "$PROJECT_DIR")"
 APP_NAME="CodexTrafficLight"
-VERSION="1.0"
+VERSION="1.0.1"
 BUNDLE="$WORKSPACE_DIR/${APP_NAME}.app"
 SOURCES="$PROJECT_DIR/Sources/main.swift"
 TMPDIR="/private/tmp/swift-cache"
